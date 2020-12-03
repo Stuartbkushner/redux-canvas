9d49bdf3834e9d6cb250f3fe732eaad14cf2d2c7
Stuart Kushner <stuartbkushner@gmail.com>
2020-12-03 11:01:53 -0500

    Can now undo and redo drawings

--- src/store.ts
+++ src/store.ts
@@ -1,9 +1,5 @@
 import { rootReducer } from "./rootReducer"
-import { composeWithDevTools } from "redux-devtools-extension"
 import { createStore, applyMiddleware } from "redux"
 import { logger } from "redux-logger"
 
-export const store = createStore(
-  rootReducer,
-  composeWithDevTools(applyMiddleware(logger))
-)
+export const store = createStore(rootReducer, applyMiddleware(logger))
